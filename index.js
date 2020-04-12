f78b68c6c887ea2ac49924b290ee2f034a413499
SkyOverZzz <skyoverlear@rambler.ru>
2020-04-12 18:21:50 +0300

    Revert "Merge branch 'master' of https://github.com/SkyOverZzz/laba"
    This reverts commit 9251b44932ef52b07e4bea9f6a289a5716e576db, reversing
    changes made to 8c619a30494610c00a5ce031023aae8546cde38b.

---
+++ index.js
@@ -0,0 +1,22 @@
+let express = require('express')
+let app = express()
+const http = require('http')
+
+app.set('view engine', 'ejs')
+
+app.use('/public', express.static('public'))
+
+app.get('/', function(req, res){
+  res.render('index')
+})
+
+app.get('/cofe', function(req, res){
+  res.render('cofe')
+})
+
+
+app.listen(3000)
+ 
+setInterval(() => {
+  http.get(http://${process.env.PROJECT_DOMAIN}.glitch.me/)
+  }, 280000)
--- public/js/preloader.js
+++
@@ -1,9 +0,0 @@
-    window.onload = function () {
-      document.body.classList.add('loaded_hiding');
-      window.setTimeout(function () {
-        document.body.classList.add('loaded');
-        console.log('preloader add')
-        document.body.classList.remove('loaded_hiding');
-        
-      }, 500);
-    }
